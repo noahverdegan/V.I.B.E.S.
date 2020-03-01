b215b3df91d6d56123031a11cd8a0f33a824c945
David Dempsey <davidgdempsey@gmail.com>
2020-03-01 14:53:40 -0800

    updated test

--- tempsensor/relayTest/relayTest.ino
+++ tempsensor/relayTest/relayTest.ino
@@ -5,6 +5,7 @@ volatile byte relayState = LOW;
 // PIR Motion Sensor is connected to D2.
 int moistPin = A0;
 
+unsigned long StartTime = millis();
 
 void setup() {
   pinMode(relay, OUTPUT);
@@ -17,9 +18,14 @@ void loop() {
   moistVal = map(moistVal-297, 274, 0, 0, 100);
   Serial.print("moist val: ");
   Serial.println(moistVal);
+  unsigned long CurrentTime = millis();
+  unsigned long ElapsedTime = CurrentTime - StartTime;
 
-  if(moistVal < 70){
+  if(moistVal < 50 && ElapsedTime > 30000){
     digitalWrite(relay, HIGH);
+    delay(1000);
+    digitalWrite(relay, LOW);
+    StartTime = millis();
   } else{
     digitalWrite(relay, LOW);
   }
